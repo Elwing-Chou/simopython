0b888e8a7ffa3d1d41b47b01966e708a106bc344
Elwing-Chou <birdfan8814@gmail.com>
2020-11-12 14:01:55 +0800

    Add files via upload

--- 1_json1.py
+++ 1_json1.py
@@ -6,17 +6,22 @@ ssl._create_default_https_context = ssl._create_unverified_context
 import json
 import os
 from urllib.request import urlopen, urlretrieve
+import pandas as pd
+from pandas.io.json import json_normalize
 
 dirname = "google"
 if not os.path.exists(dirname):
     os.makedirs(dirname)
 
-for i in range(12):
+for i in range(3):
     print("月份:", i+1)
     url = "https://www.google.com/doodles/json/2020/" + str(i+1) + "?hl=zh_TW"
     response = urlopen(url)
     pictures = json.load(response)
-
+    df = json_normalize(pictures)
+    df.to_csv("{}.csv".format(i+1),
+              encoding="utf-8",
+              index=False)
     for p in pictures:
         print(p["title"])
         imgurl = "https:" + p["url"]
