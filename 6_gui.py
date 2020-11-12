9b467a0a8f2aa36a672f6c80ede2cba5a7e74140
Elwing-Chou <birdfan8814@gmail.com>
2020-11-12 16:23:02 +0800

    Add files via upload

---
+++ 6_gui.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry("500x500+250+250")
+
+f1 = tk.Frame(window)
+f1.pack()
+
+l1 = tk.Label(f1, text="輸入體重:")
+l1.pack()
+e1 = tk.Entry(f1)
+e1.pack()
+l2 = tk.Label(f1, text="輸入身高:")
+l2.pack()
+e2 = tk.Entry(f1)
+e2.pack()
+b1 = tk.Button(f1, text="計算")
+b1.pack()
+result = tk.Label(f1, text="結果")
+result.pack()
+
+window.mainloop()
