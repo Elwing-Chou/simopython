a94a7fbb18ce33e047a0ae996bae2888acfa2e8e
Elwing-Chou <birdfan8814@gmail.com>
2020-11-12 16:31:19 +0800

    Add files via upload

--- 6_gui.py
+++ 6_gui.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 
+def bmi():
+    global e1, e2, result
+    w, h = float(e1.get()), float(e2.get())
+    b = w / (h / 100) ** 2
+    result["text"] = str(b)
+
 window = tk.Tk()
 window.geometry("500x500+250+250")
 
@@ -14,7 +20,7 @@ l2 = tk.Label(f1, text="輸入身高:")
 l2.pack()
 e2 = tk.Entry(f1)
 e2.pack()
-b1 = tk.Button(f1, text="計算")
+b1 = tk.Button(f1, text="計算", command=bmi)
 b1.pack()
 result = tk.Label(f1, text="結果")
 result.pack()
