0b5bae56357db3f4475726bcc0ecd5a769dd17cd
Elwing-Chou <birdfan8814@gmail.com>
2020-11-12 13:25:51 +0800

    Add files via upload

--- 1_json1.py
+++ 1_json1.py
@@ -11,13 +11,15 @@ dirname = "google"
 if not os.path.exists(dirname):
     os.makedirs(dirname)
 
-url = "https://www.google.com/doodles/json/2020/6?hl=zh_TW"
-response = urlopen(url)
-pictures = json.load(response)
+for i in range(12):
+    print("月份:", i+1)
+    url = "https://www.google.com/doodles/json/2020/" + str(i+1) + "?hl=zh_TW"
+    response = urlopen(url)
+    pictures = json.load(response)
 
-for p in pictures:
-    print(p["title"])
-    imgurl = "https:" + p["url"]
-    fn = imgurl.split("/")[-1]
-    total = os.path.join(dirname, fn)
-    urlretrieve(imgurl, total)
+    for p in pictures:
+        print(p["title"])
+        imgurl = "https:" + p["url"]
+        fn = imgurl.split("/")[-1]
+        total = os.path.join(dirname, fn)
+        urlretrieve(imgurl, total)
